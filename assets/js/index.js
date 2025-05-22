89fea8e9bf92cb9802377559d59593dc804e3b9e
PedroGamerRPJ <pedrohnunesabreu@gmail.com>
2025-05-22 15:53:18 -0300

    commit1 22-05

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+//Meu primeiro JS
+//fazer uma função para aparecer as armaduras
+function colocarMark1(){
+    //criar variável
+   let monstruario = document.getElementById("armaduras")
+   //monstruario.style.backgroundColor = "purple"
+    monstruario.style.backgroundImage = "url(https://i.pinimg.com/736x/89/b4/69/89b46982206f73636880666844891691.jpg)"
+    monstruario.style.backgroundSize = "cover"
+    document.body.style.backgroundColor = "gray"
+}
+
+function hulkBuster(){
+    let monstruario = document.getElementById("armaduras")
+    monstruario.style.backgroundImage = "url(https://playcontestofchampions.com/wp-content/uploads/2023/04/champion-hulkbuster.webp)"
+    monstruario.style.backgroundSize = "cover"
+    document.body.style.backgroundColor = "darkred"
+}
+
+function colocarMark34(){
+    let monstruario = document.getElementById("armaduras")
+    monstruario.style.backgroundImage = "url(https://pm1.aminoapps.com/6821/641a3fd5112a4e99290496d9f3a9b25242092fcdv2_00.jpg)"
+    monstruario.style.backgroundSize = "cover"
+    document.body.style.backgroundColor = "black"
+}
+   
